064902c71ef95f4bc7f00a7bf9cb10f77ed6d617
hchenc <happytime_ch@foxmail.com>
2021-08-27 18:48:25 +0800

    fix bug on generate_client.sh

--- generate_client.sh
+++ generate_client.sh
@@ -5,6 +5,6 @@ set -e
 GV="devops:v1alpha1"
 
 rm -rf ./pkg/client
-./hack/generate_group.sh "client,lister,informer" github.com/hchenc/pager/pkg/client github.com/hchenc/pager/apis "$GV" --output-base=./  -h "$PWD/hack/boilerplate.go.txt"
+./hack/generate_group.sh "client,lister,informer" github.com/hchenc/pager/pkg/client github.com/hchenc/pager/pkg/apis "$GV" --output-base=./  -h "$PWD/hack/boilerplate.go.txt"
 mv github.com/hchenc/pager/pkg/client ./pkg/
 rm -rf ./github.com/hchenc
